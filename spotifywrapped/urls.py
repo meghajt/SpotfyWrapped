796519b59e2f6b6c07676f9250be47e567f2e69e
Meghaj Tarte <meghajtarte33@gmail.com>
2024-10-19 16:38:58 -0400

    Done with base user story 2 except for the saved wraps. User can delete account and has to re login once they log out.

--- spotifywrapped/urls.py
+++ spotifywrapped/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path("", views.landing, name="landing"),
     path("register/", views.register, name="register"),
     path("login/", views.login_view, name="login"),
+    path('logout/', views.logout_view, name='logout'),
     path("home/", views.home, name="home"),
+    path("delete-account/", views.delete_account, name="delete_account")
 ]
--- spotifywrapped/views.py
+++ spotifywrapped/views.py
@@ -1,9 +1,13 @@
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login as auth_login
-
+from django.contrib.auth import logout
 from spotifywrapped.forms import CustomUserCreationForm
+from django.views.decorators.cache import cache_control
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
+from django.contrib import messages
 
 
 def register(request):
@@ -28,10 +32,25 @@ def login_view(request):
         form = AuthenticationForm()
     return render(request, 'registration/login.html', {'form': form})
 
+def logout_view(request):
+    logout(request)
+    request.session.flush()
+    return redirect('')
+
 def landing(request):
     return render(request, 'landing.html')
 
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
 @login_required
 def home(request):
     return render(request, 'home.html')
 
+@login_required()
+def delete_account(request):
+    if request.method == 'POST':
+        user = request.user
+        user.delete()
+        messages.success(request, "Your account has been deleted successfully.")
+        return redirect('landing')
+
+    return render(request, 'delete_account.html')
